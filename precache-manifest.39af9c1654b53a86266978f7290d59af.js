34fe57b787cf7fa9ba79424eebd326c356a105f4
ioannis-grek <57755445+ioannis-grek@users.noreply.github.com>
2020-07-09 18:31:00 +0300

    Add files via upload

---
+++ precache-manifest.39af9c1654b53a86266978f7290d59af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c709280dfaa2bccf9a1ecf4a2c1211df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b62c509d1ec09584fae1",
+    "url": "/static/css/main.f1c75924.chunk.css"
+  },
+  {
+    "revision": "15920c42bcdb9e487c81",
+    "url": "/static/js/2.93e4e6f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.93e4e6f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b62c509d1ec09584fae1",
+    "url": "/static/js/main.7ae90b7b.chunk.js"
+  },
+  {
+    "revision": "c7d54c25382efde0f9ec",
+    "url": "/static/js/runtime-main.1907fe54.js"
+  },
+  {
+    "revision": "5411bbeac18e02661efd3eadefb7da77",
+    "url": "/static/media/header.5411bbea.png"
+  }
+]);
